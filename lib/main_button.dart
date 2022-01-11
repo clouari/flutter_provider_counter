ecfcc28b68c27173c28ee448ef0fbc3aff98ffd7
Da hee <ekdudekgml35@naver.com>
2022-01-11 14:07:25 +0900

    provider import 에 src 가 들어가 있는 것을 지워도 된다.

--- lib/main_button.dart
+++ lib/main_button.dart
@@ -1,6 +1,6 @@
 import 'package:counter/main_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/src/provider.dart';
 
 class MainButton extends StatelessWidget {
   const MainButton({Key? key}) : super(key: key);
