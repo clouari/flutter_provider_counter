e10a5fa28c96f241f737d0ab811df7da20d8fe6e
Junsuk Oh <a811219@gmail.com>
2022-01-11 15:22:07 +0900

    의존성 역전 -> 해결

--- lib/main_counter.dart
+++ lib/main_counter.dart
@@ -1,15 +1,18 @@
-import 'package:counter/main_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class MainCounter extends StatelessWidget {
-  const MainCounter({Key? key}) : super(key: key);
+  final int count;
+
+  const MainCounter({
+    Key? key,
+    required this.count,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        context.watch<MainViewModel>().count.toString(),
+        '$count',
         style: const TextStyle(
           fontSize: 40,
         ),
