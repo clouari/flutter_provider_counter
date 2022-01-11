2985ddae626419538d702c088218f5bfb90b8cac
Da hee <ekdudekgml35@naver.com>
2022-01-11 14:05:37 +0900

    provider import 에 src 들어갔던 것 지웠음.

--- lib/main_counter.dart
+++ lib/main_counter.dart
@@ -1,6 +1,6 @@
 import 'package:counter/main_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/src/provider.dart';
+import 'package:provider/provider.dart';
 
 class MainCounter extends StatelessWidget {
   const MainCounter({Key? key}) : super(key: key);
