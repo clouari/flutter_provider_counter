1364491823155d0af13efa3bce55172512bb7edb
Da hee <ekdudekgml35@naver.com>
2022-01-11 12:28:23 +0900

    add 버튼과 reduce 버튼

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:counter/main_button.dart';
+import 'package:counter/main_counter.dart';
+import 'package:counter/main_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MainViewModel()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Provider"),
+      ),
+      body: ChangeNotifierProvider(
+        create: (BuildContext context) => MainViewModel(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              MainCounter(),
+              MainButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
