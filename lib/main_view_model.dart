427e1b383c43d5ca996cab1971506db081f2840a
Da hee <ekdudekgml35@naver.com>
2022-02-27 21:03:35 +0900

    Counter: ViewModel 나누기

--- lib/main_view_model.dart
+++ lib/main_view_model.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class MainViewModel with ChangeNotifier {
   int _count = 0;
   int get count => _count;
+  // 상태를 노출 하기 위해 getter (count를 창조할 때 _count 가 리턴된다.)
+  // JAVA 식에선
+  // int get count {
+  // return _count }
 
   void increment() {
     // 상태 바꾸고
