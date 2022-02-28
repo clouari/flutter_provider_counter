c037d53174cd555fafa4e4c72995a77837cb50e4
Da hee <ekdudekgml35@naver.com>
2022-02-28 18:59:20 +0900

    Counter: 하단의 버튼 기능

--- lib/main_counter.dart
+++ lib/main_counter.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class MainCounter extends StatelessWidget {
-  const MainCounter({Key? key}) : super(key: key);
+  final int count;
+
+  const MainCounter({Key? key, required this.count}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
